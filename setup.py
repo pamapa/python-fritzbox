99ce9031384310d5ea5821bb0754bd950d41da92
pamapa <pamapa@users.noreply.github.com>
2016-04-25 18:47:53 +0200

    start using setuptools

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+setup(name='python-fritzbox',
+  version='0.1',
+  description='setup the Fritz!Box with python',
+  url='https://github.com/pamapa/python-fritzbox',
+  author='Patrick Ammann',
+  author_email='pammann@gmx.net',
+  license='GNU',
+  packages=['fritzbox', 'tools'],
+  zip_safe=False,
+  scripts=[
+    os.path.join('tools', 'fritzboxutil.py'),
+    os.path.join('tools', 'fritzboxktipp.py'),
+  ]
+)
+
